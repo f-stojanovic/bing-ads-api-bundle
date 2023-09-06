0074937f05d63cd0ec25b55ab781b40cf84f3c2f
Filip Stojanovic <filip.develop@outlook.com>
2023-09-06 11:45:20 +0200

    Update BingAdsApiExtension.php

--- BingAdsApiBundle/DependencyInjection/BingAdsApiExtension.php
+++ BingAdsApiBundle/DependencyInjection/BingAdsApiExtension.php
@@ -17,7 +17,6 @@ class BingAdsApiExtension extends Extension
         $container->setParameter('bing_ads_api.developer_token', $config['developer_token']);
         $container->setParameter('bing_ads_api.api_environment', $config['api_environment']);
         $container->setParameter('bing_ads_api.oauth_scope', $config['oauth_scope']);
-        $container->setParameter('bing_ads_api.oauth_refresh_token_path', $config['oauth_refresh_token_path']);
         $container->setParameter('bing_ads_api.client_id', $config['client_id']);
         $container->setParameter('bing_ads_api.client_secret', $config['client_secret']);
         $container->setParameter('bing_ads_api.audience_id', $config['audience_id']);
