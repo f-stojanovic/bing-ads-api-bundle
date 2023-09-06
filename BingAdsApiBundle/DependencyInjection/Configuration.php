11efed5318bef7e11154873bcbde8ce32fd0a739
Filip Stojanovic <filip.develop@outlook.com>
2023-09-06 11:47:57 +0200

    Update Configuration.php

--- BingAdsApiBundle/DependencyInjection/Configuration.php
+++ BingAdsApiBundle/DependencyInjection/Configuration.php
@@ -20,7 +20,6 @@ class Configuration implements ConfigurationInterface
             ->scalarNode('developer_token')->isRequired()->end()
             ->scalarNode('api_environment')->isRequired()->end()
             ->scalarNode('oauth_scope')->isRequired()->end()
-            ->scalarNode('oauth_refresh_token_path')->isRequired()->end()
             ->scalarNode('client_id')->isRequired()->end()
             ->scalarNode('client_secret')->isRequired()->end()
             ->integerNode('audience_id')->isRequired()->end()
